e7eb082f20b883ea6f41954e4f8fee7c7489a01b
anwarendo <anwarendo@gmail.com>
2021-10-21 15:04:51 +0300

    axios practice

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 export default class App extends Component {
   render() {
     return (
-      <div>test...</div>
+      <div>test...1</div>
+      // https://www.freecodecamp.org/news/how-to-use-axios-with-react/
     )
   }
 }
