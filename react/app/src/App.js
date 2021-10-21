e767828954e128b13cd30874372034a43e7a6008
anwarendo <anwarendo@gmail.com>
2021-10-21 16:08:51 +0300

    docker networking, ... on progress

--- react/app/src/App.js
+++ react/app/src/App.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>test...1</div>
-      // https://www.freecodecamp.org/news/how-to-use-axios-with-react/
-    )
-  }
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>test...1</div>
+//       // https://www.freecodecamp.org/news/how-to-use-axios-with-react/
+//     )
+//   }
+// }
+
+import axios from "axios";
+import React from "react";
+
+// const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "http://172.18.0.6/api/posts/1";
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
 }
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/', function(){
-    return 'hello';
+Route::get('/posts', function(){
+    return response()->json([
+        'userId'=> 1,
+        'id'=> 1,
+        'title'=>"hello there",
+        'body'=>"hey there, this is the first api contact!!!",
+    ]);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
